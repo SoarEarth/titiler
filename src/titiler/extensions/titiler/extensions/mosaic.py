33324ceca633af53914f362c4fc61042399e490a
mtlacbaba <marek@takor.com.au>
2024-04-29 11:56:57 +0800

    customize output json metadata file to be use by Soar BE

--- src/titiler/extensions/titiler/extensions/mosaic.py
+++ src/titiler/extensions/titiler/extensions/mosaic.py
@@ -1,7 +1,7 @@
 """rio-stac Extension."""
 
 from dataclasses import dataclass
-from typing import Any, Dict, List, Literal, Optional
+from typing import Any, Dict, List, Literal, Optional, Tuple, Union
 
 from fastapi import Depends, Query
 from typing_extensions import Annotated, TypedDict
@@ -13,6 +13,13 @@ from fastapi import APIRouter, Body, Depends, Path, Query
 
 from titiler.core.factory import BaseTilerFactory, FactoryExtension
 from rio_tiler.io import COGReader
+import json
+import datetime
+
+from pathlib import Path
+import requests
+import morecantile
+WEB_MERCATOR_TMS = morecantile.tms.get("WebMercatorQuad")
 
 try:
     from rio_cogeo.cogeo import cog_info
@@ -20,20 +27,38 @@ try:
     import pystac
     from pystac.utils import datetime_to_str, str_to_datetime
     from rio_stac.stac import create_stac_item
-    from pystac import Catalog, Collection
+    from pystac import Catalog, Collection, Asset
 except ImportError:  # pragma: nocover
     cog_info = None  # type: ignore
     Info = None
     create_stac_item = None  # type: ignore
     pystac = None  # type: ignore
     str_to_datetime = datetime_to_str = None  # type: ignore
-    Catalog = Collection = None  # type: ignore
+    Catalog = CollectionW = None  # type: ignore
 
 
-class CreateBody(TypedDict, total=False):
-    """STAC Item."""
+class CreateBody(TypedDict):
+    """POST Body for /create endpoint."""
 
     links: List[str]
+
+class TitilerLayerMetadata(TypedDict):
+    """Titiler Layer metadata for Soar listing."""
+
+    id: str
+    title: str
+    description: str
+    stac_url: str
+    license: str
+    bboxes:  Optional[list[list[float]]]
+    intervals: Optional[list[list[str]]]
+    extra_fields: Optional[dict[str, Any]]
+    keywords: Optional[list[str]]
+    bounds: Tuple[float, float, float, float] = [-180, -90, 180, 90]
+    center: Optional[Tuple[float, float, int]]
+    min_zoom: Optional[int]
+    max_zoom: Optional[int]
+    mosaic: dict[str, Any]
 
 @dataclass
 class mosaicExtension(FactoryExtension):
@@ -68,21 +93,61 @@ class mosaicExtension(FactoryExtension):
         )
         def create_mosaic_json_from_stac_collection(
             src_path=Depends(factory.path_dependency),
-            backend_params=Depends(factory.backend_dependency),
-            reader_params=Depends(factory.reader_dependency),
-            env=Depends(factory.environment_dependency),
+            dest_path: Annotated[Optional[str], Query(description="Destination path to save the MosaicJSON.")] = None,
+            use_metadata_only: Annotated[Optional[bool], Query(description="Destination path to save the MosaicJSON.")] = False,
         ):
             """Return basic info."""
             collection = Collection.from_file(src_path)
-            # collection.make_all_asset_hrefs_absolute()
-            # return collection.to_dict()
             items = list(collection.get_items())
-            # return items
-            assets = []
-            for item in items:
-                assets.append(item.assets["visual"].get_absolute_href())
-            # return assets
-            return MosaicJSON.from_urls(assets)
+
+            items = filter(lambda item: item.assets["visual"] is not None, items)
+            data: MosaicJSON
+            if(use_metadata_only):
+                data = MosaicJSON.from_urls(assets)
+            else:
+                assets = []
+                for item in items:
+                    assets.append(item.assets["visual"].get_absolute_href())
+                data = MosaicJSON.from_urls(assets)
+
+            print(F"dest: {dest_path} - {dest_path is not None}")
+            if(dest_path is not None):
+                print(F"dest-startWith: {dest_path.startswith('/data/')} - {dest_path.startswith('https://')}")
+
+            # map datetime into ISO format
+            mapped_intervals : list[list[str]] = []
+            for interval in collection.extent.temporal.intervals:
+                mapped_intervals.append([datetime_to_str(interval[0]), datetime_to_str(interval[1])])
+
+            metadata : TitilerLayerMetadata = {
+                "id": collection.id,
+                "title": collection.title,
+                "description": collection.description,
+                "stac_url": collection.get_self_href(),
+                "license": collection.license,
+                "bboxes": collection.extent.spatial.bboxes,
+                "intervals": mapped_intervals,
+                "extra_fields": collection.extra_fields,
+                "keywords": collection.keywords,
+                "mosaic": data.model_dump(),
+                "bounds": data.bounds,
+                "center": data.center,
+                "min_zoom": data.minzoom,
+                "max_zoom": data.maxzoom
+            }
+
+
+
+            if(dest_path is not None and (dest_path.startswith("/data/") or dest_path.startswith("data/"))):
+                output_file = Path(f"{dest_path}/metadata/{collection.id.lower()}.json")
+                output_file.parent.mkdir(exist_ok=True, parents=True)
+                output_file.write_text(json.dumps(metadata))
+                return 'MosaicJSON saved to ' + dest_path
+            elif (dest_path is not None and dest_path.startswith("https://")):
+                requests.post(dest_path, data = json.dumps(metadata))
+                return 'MosaicJSON sent as POST request to ' + dest_path
+            else:
+                return metadata
 
         @factory.router.get(
             "/createFromStacCatalog", 
